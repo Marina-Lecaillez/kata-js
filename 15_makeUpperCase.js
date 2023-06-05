934219abf60bb24431f0fca7df43ef202966b647
Marina-Lecaillez <lecaillez.marina@gmail.fr>
2023-06-05 15:35:33 +0200

    15 ok

---
+++ 15_makeUpperCase.js
@@ -0,0 +1,9 @@
+// Write a function which converts the input string to uppercase.
+// function makeUpperCase(str) {
+//     // Code here
+//   }
+
+function makeUpperCase(str) {
+    // Code here
+    return (str.toUpperCase());
+  }
